ddc6ca2af22bd16ceeba66dbbacbc0b8743ea636
trungggg48-afk <trungggg48@gmail.com>
2026-07-09 04:53:20 +0700

    Update Main.java

--- src/main/java/me/example/myplugin/Main.java
+++ src/main/java/me/example/myplugin/Main.java
@@ -1,1 +1,27 @@
+package me.example.myplugin;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("MyPlugin da bat!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("MyPlugin da tat!");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("hello")) {
+            sender.sendMessage("Xin chao tu plugin rieng cua ban!");
+            return true;
+        }
+        return false;
+    }
+}
